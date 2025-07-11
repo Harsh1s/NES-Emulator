1e226cd440bca2bfb4f8f04ebebc92b57199b8d9
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-07-11 19:13:10 +0530

    Decode BRK and NOP opcodes

--- src/debug/opcode.rs
+++ src/debug/opcode.rs
@@ -1,3 +1,6 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum AddressingMode { Implied, Immediate, ZeroPage, Absolute, Relative }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct OpcodeInfo { pub opcode: u8, pub mnemonic: &'static str, pub mode: AddressingMode, pub cycles: u8 }
+
