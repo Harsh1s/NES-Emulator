4ea5197cc8c32baebfdf56f21ee7eac0ede2254c
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-08-02 08:24:54 +0530

    Decode absolute JMP opcode

--- src/debug/cartridge.rs
+++ src/debug/cartridge.rs
@@ -1,3 +1,6 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Mirroring { Horizontal, Vertical, FourScreen }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct HeaderSummary { pub prg_rom_banks: u8, pub chr_rom_banks: u8, pub mapper: u8, pub mirroring: Mirroring }
+
